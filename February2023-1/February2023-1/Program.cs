87fb48fed4dcf957713843769e71af070647884b
unknown <reebababu1@gmail.com>
2023-03-10 15:43:46 +1300

    edit and delete code added

--- February2023-1/February2023-1/Program.cs
+++ February2023-1/February2023-1/Program.cs
@@ -44,12 +44,13 @@ createNew.Click();
 Thread.Sleep(1000);
 //input code into code textbox
 IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
-typeCodeDropdown.Click(); 
+typeCodeDropdown.Click();
+Thread.Sleep(1000);
 IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
 timeOption.Click();
 //input code into code textbox
-IWebElement codetextbox = driver.FindElement(By.Id("Code"));
-codetextbox.SendKeys("February2023");
+IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+codeTextbox.SendKeys("February2023");
 //input descriptionin into description textbox
 IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
 descriptionTextbox.SendKeys("February2023");
@@ -75,3 +76,56 @@ else
 {
     Console.WriteLine("new code not created");
 }
+//Edit time record
+IWebElement lastPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+lastPage.Click();
+Thread .Sleep(500);
+IWebElement edit = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+edit.Click(); 
+Thread .Sleep(500);
+IWebElement editCode = driver.FindElement(By.Id("Code"));
+editCode.Clear();
+editCode.SendKeys("March2023");
+//New edit code save
+IWebElement editCodeSave = driver.FindElement(By.Id("SaveButton"));
+editCodeSave.Click();
+Thread .Sleep(1000);
+//check new code is modified
+//go to last page
+IWebElement gotoLastPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+gotoLastPage.Click();
+Thread.Sleep(2000);
+//check test code edited
+IWebElement testEdit = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+if (testEdit.Text == "March2023")  
+{
+    Console.WriteLine("test code is edited sucessfully");
+}
+else
+{
+    Console.WriteLine("test code not edited");
+}
+//Delete code
+// Go to last page
+IWebElement lastPage1 = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+lastPage1.Click();
+Thread.Sleep(500);
+//delete code
+IWebElement deleteCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
+deleteCode.Click();
+Thread.Sleep(1000);
+//click ok to the popup delete button
+IAlert aL = driver.SwitchTo().Alert();
+aL.Accept();
+Thread.Sleep(2000);
+IWebElement testDelete = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+if(testDelete.Text != "March2023")
+{
+    Console.WriteLine("Record is deleted");
+}
+else
+{
+    Console.WriteLine("record is not deletrd");
+}
+
+
